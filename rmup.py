189b95f3be16e72da3e1616f2b39ece500118006
Rainer Bareiß <Heavy02011@users.noreply.github.com>
2018-02-03 14:06:26 +0100

    initial commit

---
+++ rmup.py
@@ -0,0 +1,14 @@
+# https://www.digitalocean.com/community/tutorials/how-to-use-web-apis-in-python-3
+
+
+
+
+# https://www.analyticsvidhya.com/blog/2015/08/data-scientist-meetup-hack/
+
+import urllib
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+from geopy.geocoders import Nominatim
+
+
